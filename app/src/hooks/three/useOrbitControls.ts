64cecc4999cc5585aef94816b942fbb4b689d77e
Milenasuvajac <milenasuvajac@hotmail.com>
2025-09-22 13:27:58 +0200

    feat: limit pan, set new start camera position

--- app/src/hooks/three/useOrbitControls.ts
+++ app/src/hooks/three/useOrbitControls.ts
@@ -5,16 +5,16 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 export const useOrbitControls = (camera : THREE.PerspectiveCamera, renderer : THREE.WebGLRenderer) => {
 
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;  // smooth movement
-    controls.dampingFactor = 0.08;
-    controls.enablePan = true;      // allow moving scene
-    controls.enableZoom = true;     // allow zoom with scroll
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.07;
+    controls.enablePan = true;
+    controls.enableZoom = true;
     controls.enableRotate = true;
-    controls.target.set( -0.00763346160769606, 0.9865404956019542, 0.2127792947689923);
+    controls.target.set( 27.041438298632446, 2.5881179106863392, 14.655313036686588);
     controls.minPolarAngle = 0;
     controls.maxPolarAngle = Math.PI / 2 + 0.15;
     controls.minAzimuthAngle = 0
-    controls.maxAzimuthAngle = Math.PI / 2 + 1.5;
+    controls.maxAzimuthAngle = Math.PI / 2 + 0.8;
     controls.minDistance = 1;
     controls.maxDistance = 95;
     controls.mouseButtons.LEFT = THREE.MOUSE.ROTATE;
@@ -31,6 +31,20 @@ export const useOrbitControls = (camera : THREE.PerspectiveCamera, renderer : TH
         ONE: THREE.TOUCH.ROTATE,
         TWO: THREE.TOUCH.DOLLY_PAN
     }
+    const panLimits = {
+        minX: -5,
+        maxX: 5,
+        minY: 3,
+        maxY: 10,
+        minZ: -5,
+        maxZ: 5
+    };
+
+    controls.addEventListener('change', () => {
+        controls.target.x = Math.max(panLimits.minX, Math.min(panLimits.maxX, controls.target.x));
+        controls.target.y = Math.max(panLimits.minY, Math.min(panLimits.maxY, controls.target.y));
+        controls.target.z = Math.max(panLimits.minZ, Math.min(panLimits.maxZ, controls.target.z));
+    });
 
     return controls;
 }
