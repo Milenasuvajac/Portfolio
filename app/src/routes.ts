58a85027be5236eeeed5d6877761121a657ef09d
Milenasuvajac <milenasuvajac@hotmail.com>
2025-09-23 15:45:47 +0200

    fix: login redirect

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -7,6 +7,6 @@ export const publicRoutes: string[] = [
     '/aboutme/public'
 ]
 // Redirect URL for regular users after login or access
-export const DEFAULT_USER_REDIRECT_URL = '/'
+export const DEFAULT_USER_REDIRECT_URL = '/aboutme/public'
 // Redirect URL for admin users after login or access
 export const DEFAULT_ADMIN_REDIRECT_URL = '/admin'
