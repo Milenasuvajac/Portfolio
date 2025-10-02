b6c5702ae0f8b1602ce3bfbc189f43d8f3a1e06c
Milenasuvajac <milenasuvajac@hotmail.com>
2025-10-02 14:29:42 +0200

    fix: temporarily remove everything from middleware

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -1,25 +1,6 @@
-import NextAuth from 'next-auth'
+import { NextResponse } from "next/server"
 
-import authConfig from './auth.config'
-
-
-// Get the authentication secret from environment variables
-const secret = process.env.AUTH_SECRET
-if (!secret) {
-  throw new Error('Secret is missing')
+export function middleware() {
+    return NextResponse.next()
 }
 
-// Initialize NextAuth with the configuration
-const { auth } = NextAuth(authConfig)
-
-
-export default auth(async req => {
-
-
-    return undefined
-})
-// Configuration for the middleware, specifying which paths to match
-export const config = {
-    // every single page except _next static files are going to invoke middleware
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
