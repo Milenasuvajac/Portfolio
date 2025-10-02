d9620754b162f6edd073f2e967f42000ffee6aad
Milenasuvajac <milenasuvajac@hotmail.com>
2025-10-02 14:07:39 +0200

    fix: temporarily remove checks in middleware.ts

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -1,12 +1,7 @@
-import { NextResponse } from 'next/server'
 import NextAuth from 'next-auth'
-import { getToken } from 'next-auth/jwt'
 
 import authConfig from './auth.config'
-import {
-    authRoutes,
-    DEFAULT_USER_REDIRECT_URL, publicRoutes,
-} from './routes'
+
 
 // Get the authentication secret from environment variables
 const secret = process.env.AUTH_SECRET
@@ -19,30 +14,8 @@ const { auth } = NextAuth(authConfig)
 
 
 export default auth(async req => {
-    const { nextUrl } = req
-    const isLoggedIn = !!req.auth
-    const token = await getToken({ req, secret })
-    const isUserAdmin = token?.role === 'ADMIN'
 
-    const isAuthPage = authRoutes.includes(nextUrl.pathname)
-    const isAdminPage = nextUrl.pathname.includes('/admin')
-    const isPublicPage = publicRoutes.includes(nextUrl.pathname)
 
-    if(isAuthPage && isLoggedIn) {
-        return NextResponse.redirect(new URL(DEFAULT_USER_REDIRECT_URL, nextUrl))
-    }
-
-    if (!isLoggedIn && !isAuthPage && !isPublicPage) {
-        return NextResponse.redirect(new URL('/auth/login', nextUrl))
-    }
-    if(isLoggedIn && nextUrl.pathname.includes('aboutme/public')) {
-        return NextResponse.redirect(new URL('/aboutme/private', nextUrl))
-
-    }
-
-    if (isAdminPage && !isUserAdmin) {
-        return NextResponse.redirect(new URL(DEFAULT_USER_REDIRECT_URL, nextUrl))
-    }
     return undefined
 })
 // Configuration for the middleware, specifying which paths to match
